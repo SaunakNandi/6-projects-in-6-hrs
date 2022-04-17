d950a8f7289f87c76170dc31d84422443f596858
Saunak Nandi <63156949+SaunakNandi@users.noreply.github.com>
2022-04-17 15:54:24 +0530

    Update script.js

--- movie template app/script.js
+++ movie template app/script.js
@@ -1,3 +1,8 @@
+const apiUrl =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const imgpath = "https://image.tmdb.org/t/p/w1280";
+const searchApi =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main=document.querySelector('main')
 const form=document.querySelector('#form')
